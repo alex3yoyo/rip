ac21b2f40f0eac3870c8ed502e5ffe0967ac0eed
alex3yoyo <alex3yoyo@gmail.com>
2013-08-06 15:44:20 -0400

    added more to the "test"

---
+++ test/gonewild/update.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+sort_file_date() {
+    # To avoid renamed files overwriting non-renamed files, files must be named twice
+    # First round of renaming: TEMPNAME-123_user.jpg
+    local n=1
+    for q in `ls -t`; do
+        local ext="${q##*.}"
+        local count=$(printf %03d $n)
+        local newname="TEMPNAME-${count}_${i}.${ext}"
+        mv "$q" "$newname"
+        local n=$(($n+1))
+    done
+    # Second (and final) round of renaming: 123_user.jpg
+    local n=1
+    for q in `ls -t`; do
+        local ext="${q##*.}"
+        local count=$(printf %03d $n)
+        local newname="${count}_${i}.${ext}"
+        mv "$q" "$newname"
+        local n=$(($n+1))
+    done
+}
+
+# Get the list of users, store to USERS
+USERS=$(<users.txt)
+
+for i in $USERS; do
+        echo "${i}"
+
+        if [[ ! -d "rips/${i}" ]]; then
+            mkdir "rips/${i}"
+        fi
+        if [[ ! -f "lists/${i}.txt" ]]; then
+            touch "lists/${i}.txt"
+        fi
+
+        # Get URLs of all images for the user
+        echo "Checking for new photos from ${i}"
+        URL="http://getgonewild.com/profile/${i}/"
+        (../../rip.cgi "${URL}" "false" "true")
+        wait
+
+        mv "rips/getgonewild${i}.txt" "lists/${i}.new.txt"
+        mv "lists/${i}.txt" "lists/${i}.old.txt"
+        # Sort user.old.txt and user.new.txt, compare for differences, write new URLs to user.geturls.txt
+        comm -13 <(sort lists/${i}.old.txt) <(sort lists/${i}.new.txt) > "lists/${i}.geturls.txt"
+
+        # Download new images
+        echo "Downloading new photos for ${i}"
+        cd "rips/${i}"
+        # Calculate number of images to download
+        total=1
+        for k in $(<../../lists/${i}.geturls.txt); do
+            total=$(($total+1))
+        done
+        # Download
+        counting=1
+        for k in $(<../../lists/${i}.geturls.txt); do
+            echo "Getting ${counting}/${total}"
+            wget --no-clobber --no-verbose --tries=25 --waitretry=1 "${k}"
+            echo "Got ${counting}"
+            counting=$(($counting+1))
+        done
+        wait
+        echo "Renaming files"
+        sort_file_date
+        cd ../../
+
+        touch "lists/${i}.txt"
+        cat "lists/${i}.geturls.txt" "lists/${i}.old.txt" > "lists/${i}.txt"
+        rm "lists/${i}.new.txt" "lists/${i}.old.txt" "lists/${i}.geturls.txt"
+done
+
+rm "recent_rips.lst"
+
+exit
--- test/update.sh
+++ test/update.sh
@@ -1,53 +1,11 @@
 #!/bin/bash
 
-sort_file_date() {
-    local n=1
-    for q in `ls -t`; do
-        local ext="${q##*.}"
-        local count=$(printf %03d $n)
-        local newname="${count}_${i}.${ext}"
-        mv "$q" "$newname"
-        local n=$(($n+1))
-    done
-    wait
-}
+echo "Updateing gonewild users"
+./gonewild/update.sh
 
-USERS=$(<users.txt)
+echo "Updating instagram users"
+./instagram/update.sh
 
-for i in $USERS; do
-        echo "${i}"
+echo "Done"
 
-        if [[ ! -d "rips/${i}" ]]; then
-            mkdir "rips/${i}"
-        fi
-        if [[ ! -f "lists/${i}.txt" ]]; then
-            touch "lists/${i}.txt"
-        fi
-
-        echo "Checking for new photos from ${i}"
-        URL="http://web.stagram.com/n/${i}/"
-        (../rip.cgi "${URL}" "false" "true")
-        wait
-
-        mv "rips/instagram_${i}.txt" "lists/${i}.new.txt"
-        mv "lists/${i}.txt" "lists/${i}.old.txt"
-        comm -13 <(sort lists/${i}.old.txt) <(sort lists/${i}.new.txt) > "lists/${i}.geturls.txt"
-
-        echo "Downloading new photos for ${i}"
-        cd "rips/${i}"
-        for k in $(<../../lists/${i}.geturls.txt); do
-            wget --no-verbose "${k}" &
-        done
-        wait
-        sort_file_date
-        cd ../../
-
-        touch "lists/${i}.txt"
-        cat "lists/${i}.geturls.txt" "lists/${i}.old.txt" > "lists/${i}.txt"
-        rm "lists/${i}.new.txt" "lists/${i}.old.txt" "lists/${i}.geturls.txt"
-done
-
-wait
-rm "recent_rips.lst"
-
-# tail -r ../recent_rips.lst | egrep -m 1 .
+exit
